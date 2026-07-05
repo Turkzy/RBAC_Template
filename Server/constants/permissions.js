6237e35fd631fadab32e76ddf71b1780a8d23591
Turkzy <sisonjohnalbert0422@gmail.com>
2026-07-05 21:29:16 +0800

    test

--- Server/constants/permissions.js
+++
@@ -1,7 +0,0 @@
-export const PERMISSIONS = {
-    USERS_VIEW: "users.view",
-    USERS_UPDATE: "users.update",
-    USERS_DELETE: "users.delete",
-    ROLES_MANAGE: "roles.manage",
-    PERMISSIONS_MANAGE: "permissions.manage",
-}
