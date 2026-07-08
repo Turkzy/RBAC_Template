18b799171ed512aa2e3340bce46f100a225e3e9d
MKP824 <sisonjohnalbert0422@gmail.com>
2026-07-08 14:30:40 +0800

    test

--- Server/routes/UnitRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllUnits } from "../controllers/UnitController.js";
-import { authMiddleware } from "../middleware/authmiddleware.js";
-
-const router = express.Router();
-
-router.get("/get-units", authMiddleware, getAllUnits);
-
-export default router;
